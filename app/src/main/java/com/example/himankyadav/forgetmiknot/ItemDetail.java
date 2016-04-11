96d6d9a7b054159a79ca5ab888550536a76f9f4b
Himank Yadav <him229@gmail.com>
2016-04-11 05:40:23 -0500

    min working

---
+++ app/src/main/java/com/example/himankyadav/forgetmiknot/ItemDetail.java
@@ -0,0 +1,21 @@
+package com.example.himankyadav.forgetmiknot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class ItemDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+    public void viewonmapClick(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/himankyadav/forgetmiknot/Main2Activity.java
@@ -0,0 +1,97 @@
+package com.example.himankyadav.forgetmiknot;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+import static java.util.Objects.*;
+
+public class Main2Activity extends AppCompatActivity {
+
+    ListView lv;
+    String[] itemname ={
+            "Mini Cooper",
+            "Tesla 3",
+            "BMW Series 5",
+            "Toyota Camry",
+            "Ford Fusion",
+            "Mercedes 340",
+            "Porsche Carrera",
+            "Nissan Sentra"
+    };
+
+    Integer[] imgid={
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+            R.drawable.nano,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Will lead to a screen to add a new Knot", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        lv = (ListView) findViewById(R.id.listView);
+//        lv.setAdapter(new ArrayAdapter<String>(
+//                this, R.layout.mylist,
+//                R.id.Itemname,itemname));
+
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        lv.setAdapter(adapter);
+        addListenerToList();
+
+
+    }
+
+    public void addListenerToList(){
+        final Context context = this;
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,ItemDetail.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
